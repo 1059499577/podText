46f25876c77ceafed66206c4756277283256cd94
RenXiangDong <1059499577@qq.com>
2017-02-28 17:29:20 +0800

    asdfgh

---
+++ XMRXD/Class/XMLog.h
@@ -0,0 +1,13 @@
+//
+//  XMLog.h
+//  XMRXD
+//
+//  Created by RenXiangDong on 17/2/28.
+//  Copyright © 2017年 RenXiangDong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XMLog : NSObject
+- (void)xmLog;
+@end
---
+++ XMRXD/Class/XMLog.m
@@ -0,0 +1,15 @@
+//
+//  XMLog.m
+//  XMRXD
+//
+//  Created by RenXiangDong on 17/2/28.
+//  Copyright © 2017年 RenXiangDong. All rights reserved.
+//
+
+#import "XMLog.h"
+
+@implementation XMLog
+- (void)xmLog {
+    NSLog(@"111");
+}
+@end
