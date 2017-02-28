eb13c548e6316df9918663cade8c5730c5438aaa
RenXiangDong <1059499577@qq.com>
2017-02-28 17:27:09 +0800

    没有

---
+++ XMRXD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMRXD
+//
+//  Created by RenXiangDong on 17/2/28.
+//  Copyright © 2017年 RenXiangDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMRXD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMRXD
+//
+//  Created by RenXiangDong on 17/2/28.
+//  Copyright © 2017年 RenXiangDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
